0a135cf9b0d24e0056428e1149bb643fa373f5ff
Abhishek-tara <abhishektara.ak47@gmail.com>
2023-05-10 08:37:38 +0530

    add: Generic demo

---
+++ GenericDemo/DemoGenericClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericDemo
+{
+    public class DemoGenericClass<T>
+    {
+        //variable of type T
+        public T data;
+
+        //Generic constructor
+        public DemoGenericClass(T data)
+        {
+            this.data = data;
+            Console.WriteLine("Data :" + this.data);
+        }
+        
+        public void Method(T data)
+        {
+            Console.WriteLine("Data :" + data);
+        }
+        //Generic Method
+        public void DemoMethod<T>(T data)
+        {
+            Console.WriteLine("Generic Data :" + data);
+        }
+    }
+}
---
+++ GenericDemo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericDemo
+{
+    //Generic means the general form.
+    //It doesn't contain any data type.
+    //Generic means not a specific to a particular data type.
+    //we use <> bracket for placeholders with Type name.
+    //TypeName<T> T is your data type parameter.
+    //Generic allow us to create a single class or method that can be used with different data type.
+    //Advantage: We can reuse our code.
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            DemoGenericClass<string> demo = new DemoGenericClass<string>("Bruce");
+            demo.Method("string");
+            demo.DemoMethod<int>(8);
+            demo.DemoMethod<string>("Bruce");
+
+            DemoGenericClass<int> intObject = new DemoGenericClass<int>(8);
+            intObject.Method(45);
+
+        }
+    }
+}
